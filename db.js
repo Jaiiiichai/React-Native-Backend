571eefcf0b4dfb0ec2c6e96a04f1ee2a8b7aeeda
xylerjace <jaydicdiquin@gmail.com>
2024-10-19 01:43:16 +0800

    cloud database

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
-// db.js
-const { Pool } = require('pg');
+const mysql = require('mysql2/promise');
 require('dotenv').config(); // Load environment variables
 
 // Create a pool of connections
-const pool = new Pool({
-    user: process.env.DB_USER,
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 // Export the pool for use in your application
